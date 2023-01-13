ab41ec8685cbd0d6b619e4d7860c42bc3ff46541
PhanThiVy <phanthithaovy2001@gmail.com>
2023-01-13 20:48:12 +0700

    update file CustomerResourceLoader

--- src/main/java/com/example/ResourceDemmo/CustomResourceLoader.java
+++ src/main/java/com/example/ResourceDemmo/CustomResourceLoader.java
@@ -13,17 +13,16 @@ import org.springframework.stereotype.Component;
  *
  * @author DELL
  */
-public class CustomResourceLoader{
-//        implements ResourceLoaderAware {
+public class CustomResourceLoader implements ResourceLoaderAware {
 
-//    private ResourceLoader resourceLoader;
-//
-//    @Override
-//    public void setResourceLoader(ResourceLoader resourceLoader) {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-//    }
-//
-//    public Resource getResource(String location) {
-//        return resourceLoader.getResource(location);
-//    }
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Resource getResource(String location) {
+        return resourceLoader.getResource(location);
+    }
 }
